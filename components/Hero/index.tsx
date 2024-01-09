41bbf644fe2bd93e44fc78ad633a32f5e569e14b
Csaba Varga <31925767+csvarga@users.noreply.github.com>
2024-01-09 19:37:11 +0100

    v1

---
+++ components/Hero/index.tsx
@@ -0,0 +1,13 @@
+import Link from "next/link";
+
+export const Hero = () => {
+  return (
+    <section className="container mx-auto py-12 text-center border-b">
+      <div>
+        <h1 className="text-2xl md:text-4xl">
+          Hey, this is the <strong>first headless WP blog</strong>!
+        </h1>
+      </div>
+    </section>
+  );
+};

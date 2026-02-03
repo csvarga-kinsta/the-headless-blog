6a468260a33237890932914af001fb83bdee4292
csvarga-kinsta <156122895+csvarga-kinsta@users.noreply.github.com>
2026-02-03 15:01:48 +0100

    dependency updates with fix

--- lib/base.ts
+++ lib/base.ts
@@ -1,9 +1,13 @@
-const API_URL = <string>process.env.NEXT_PUBLIC_WORDPRESS_API_ENDPOINT;
+const API_URL = process.env.NEXT_PUBLIC_WORDPRESS_API_ENDPOINT;
 
 export async function fetchAPI(
   query = "",
   { variables }: Record<string, any> = {}
 ) {
+  if (!API_URL) {
+    throw new Error('NEXT_PUBLIC_WORDPRESS_API_ENDPOINT is not set');
+  }
+
   const headers = { "Content-Type": "application/json" };
 
   const res = await fetch(API_URL, {
